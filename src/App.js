e08a6143a2a5a9da2154fef2d0987d0294243a6a
CarolPazini <45315401+carolpazini@users.noreply.github.com>
2019-04-19 14:47:36 -0300

    consegui colocar o tabuleiro

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
+//aqui esta o tabuleiro
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="tabuleiro">
+ 
+        <div id="a1" className="bloco" onclick="jogada(this.id);verifica();"></div>
+        <div id="a2" className="bloco" onclick="jogada(this.id);verifica();"></div>
+        <div id="a3" className="bloco" onclick="jogada(this.id);verifica();"></div>
+
+        <div id="b1" className="bloco" onclick="jogada(this.id);verifica();"></div>
+        <div id="b2" className="bloco" onclick="jogada(this.id);verifica();"></div>
+        <div id="b3" className="bloco" onclick="jogada(this.id);verifica();"></div>
+
+        <div id="c1" className="bloco" onclick="jogada(this.id);verifica();"></div>
+        <div id="c2" className="bloco" onclick="jogada(this.id);verifica();"></div>
+        <div id="c3" className="bloco" onclick="jogada(this.id);verifica();"></div>
+         
+</div>
     );
   }
 }
 
+//aqui estao definidos dos bloquinhos- eu acho
+class Bloco extends React.Component{
+  render(){
+    return(
+      <button className="bloco">
+      {this.props.value}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
 export default App;
