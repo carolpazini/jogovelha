f7caf5546a700486f9e2e14ad8f8f185795469de
CarolPazini <45315401+carolpazini@users.noreply.github.com>
2019-04-21 12:07:37 -0300

    tentando comecar a ver quem ganha

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ class Bloco extends React.Component {
                 ? null
                 : this.state.value === "_"
                 ? () => {
-                    this.setState({ value: jogada.state.nextPlayer }, () => {
-                      jogada.state.toggleNextPlayer();
+                    this.setState({ value: jogada.state.proxJogador }, () => {
+                      jogada.state.toggleProxJogador();
                     });
                   }
                 : null
@@ -52,16 +52,16 @@ function Tabuleiro() {
         <>
           <div className="tabuleiro">
             <div className="bloco"> <Bloco ref={jogada.state.a1} /> </div>
-            <div className="bloco"><Square ref={game.state.a2} /> </div>
-            <div className="bloco"><Square ref={game.state.a3} /> </div>
+            <div className="bloco"><Bloco ref={jogada.state.a2} /> </div>
+            <div className="bloco"><Bloco ref={jogada.state.a3} /> </div>
           
-            <div className="bloco"> <Square ref={game.state.b1} /> </div>
-            <div className="bloco"> <Square ref={game.state.b2} /> </div>
-            <div className="bloco"> <Square ref={game.state.b3} /> </div>
+            <div className="bloco"> <Bloco ref={jogada.state.b1} /> </div>
+            <div className="bloco"> <Bloco ref={jogada.state.b2} /> </div>
+            <div className="bloco"> <Bloco ref={jogada.state.b3} /> </div>
           
-            <div className="bloco"> <Square ref={game.state.c1} /> </div>
-            <div className="bloco"> <Square ref={game.state.c2} /> </div>
-            <div className="bloco"> <Square ref={game.state.c3} /> </div>
+            <div className="bloco"> <Bloco ref={jogada.state.c1} /> </div>
+            <div className="bloco"> <Bloco ref={jogada.state.c2} /> </div>
+            <div className="bloco"> <Bloco ref={jogada.state.c3} /> </div>
           </div>
         </>
       )}
@@ -76,24 +76,69 @@ class Jogada extends React.Component {
     super();
     this.state = {
       move: 0,
-      nextPlayer: "X",
-      toggleNextPlayer: this.toggleNextPlayer,
+      proxJogador: "X",
+      toggleProxJogador: this.toggleProxJogador,
       progress: "Em jogo"
     };
   }
 
+  componentDidMount() {
+    console.log(this.state.a1);
+    this.setState(
+      {
+        a1: React.createRef(),
+        a2: React.createRef(),
+        a3: React.createRef(),
+        b1: React.createRef(),
+        b2: React.createRef(),
+        b3: React.createRef(),
+        c1: React.createRef(),
+        c2: React.createRef(),
+        c3: React.createRef()
+      },
+      () => {
+        console.log(this.state.a1);
+      }
+    );
+  }
 
-/* aqui estao definidos dos bloquinhos- eu acho
-class Bloco extends React.Component{
-  render(){
+  toggleProxJogador = () => {
+    this.setState(
+      {
+/* poderia ser algo mais ou menos assim tbm eu acho
     const icone = this.state;
     if(icone === ankh){
       return ankh;
     }else{
       return horus; 
-  }
-}
-} */
+ */
+        proxJogador: this.state.proxJogador === "X" ?  "O" : "X",
+        move: this.state.move++
+      },
+      this.verifGanhador()
+    );
+  };
+
+//verifica na real quem ganhou
+verifGanhador = () => {
+  let a1 = this.state.a1.current.state.value;
+  let a2 = this.state.a2.current.state.value;
+  let a3 = this.state.a3.current.state.value;
+
+  let b1 = this.state.b1.current.state.value;
+  let b2 = this.state.b2.current.state.value;
+  let b3 = this.state.b3.current.state.value;
+
+  let c1 = this.state.c1.current.state.value;
+  let c2 = this.state.c2.current.state.value;
+  let c3 = this.state.c3.current.state.value;
+
+//colocar as probabilidades
+
+};
+
+
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Jogada />, rootElement);
